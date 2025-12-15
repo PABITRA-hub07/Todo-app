9109fd36cbffa42226d91da0d6336648a47f4235
Pabitra Dey <pabitradey0366@gmail.com>
2025-12-15 19:56:15 +0530

    Complete frontend CRUD functionality

--- backend/server.js
+++ backend/server.js
@@ -7,9 +7,50 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+//Data
+let todos = [];
+
 //Test route
-app.get("/", (req, res) => {
-  res.send("Running...");
+app.get("/todos", (req, res) => {
+  res.json(todos);
+});
+app.post("/todos", (req, res) => {
+  const text = req.body.text;
+
+  const newTodo = {
+    id: Date.now(),
+    text: text,
+    complete: false,
+  };
+
+  todos.push(newTodo);
+  res.status(201).json(newTodo);
+});
+
+app.put("/todos/:id", (req, res) => {
+  const id = Number(req.params.id);
+
+  const todo = todos.find((t) => t.id === id);
+
+  if (!todo) {
+    return res.status(404).json({ message: "Todo not found" });
+  }
+
+  todo.completed = true;
+  res.json(todo);
+});
+
+app.delete("/todos/:id", (req, res) => {
+  const id = Number(req.params.id);
+
+  const index = todos.findIndex((t) => t.id === id);
+
+  if (index === -1) {
+    return res.status(404).json({ message: "Todo not found" });
+  }
+
+  todos.splice(index, 1);
+  res.json({ message: "Todo deleted" });
 });
 
 const PORT = 5000;
